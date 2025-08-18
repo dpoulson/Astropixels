57db9e6ef2b9f8d33e3d9b4a91a0ddfb8184161f
Darren Poulson <darren.poulson@gmail.com>
2025-08-18 15:49:26 +0100

    First commit

---
+++ src/r2kt/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include "ReelTwo.h"
+#include "dome/Logics.h"
+#include "dome/LogicEngineController.h"
+#include "dome/HoloLights.h"
+#include "dome/NeoPSI.h"
+#include "i2c/I2CReceiver.h"
+
+
+I2CReceiver i2cReceiver(0x0a);
+
+AstroPixelRLD<> RLD(LogicEngineRLDDefault, 3);
+AstroPixelFLD<> FLD(LogicEngineFLDDefault, 1);
+
+AstroPixelFrontPSI<> frontPSI(LogicEngineFrontPSIDefault, 4);
+AstroPixelRearPSI<> rearPSI(LogicEngineRearPSIDefault, 5);
+
+HoloLights frontHolo(25, HoloLights::kRGB);
+HoloLights rearHolo(26, HoloLights::kRGB);
+HoloLights topHolo(27, HoloLights::kRGB);   
+
+void setup()
+{
+    REELTWO_READY();
+    SetupEvent::ready();
+    RLD.selectScrollTextLeft("... AstroPixels ....", LogicEngineRenderer::kBlue, 0, 15);
+    FLD.selectScrollTextLeft("... R2D2 ...", LogicEngineRenderer::kRed, 0, 15);
+    CommandEvent::process("HPA0026|20");
+}
+
+void loop() {
+  AnimatedEvent::process();
+}
